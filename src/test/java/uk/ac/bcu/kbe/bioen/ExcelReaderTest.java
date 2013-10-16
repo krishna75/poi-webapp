3d8c1960adcdd2ffbe9fc62a3c0a2b2599c50c1b
Krishna Sapkota <krishna.sapkota@bcu.ac.uk>
2013-10-16 17:12:16 +0100

    testing a values of a row

--- src/test/java/uk/ac/bcu/kbe/bioen/ExcelReaderTest.java
+++ src/test/java/uk/ac/bcu/kbe/bioen/ExcelReaderTest.java
@@ -1,11 +1,15 @@
 package uk.ac.bcu.kbe.bioen;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import uk.ac.bcu.kbe.bioen.model.ExcelReader;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -18,14 +22,34 @@ import static org.junit.Assert.assertEquals;
 
 
 public class ExcelReaderTest {
-    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
-    public void testGetSomething() throws Exception {
+    public void testGetRowValues() throws Exception {
 
         String filename = this.getClass().getClassLoader().getResource("test.xlsx").getFile();
-        log.debug(filename);
-        assertEquals("something", new ExcelReader(filename).getSomething());
+
+        assertTrue(contains(new ExcelReader(filename).getRowValues(0, 4)
+                ,(Lists.newArrayList("Name", "Address", "Phone", "Email"))));
+        assertTrue(contains(new ExcelReader(filename).getRowValues(0, 5)
+                ,(Lists.newArrayList("Krishna Sapkota", "303 Hollow Way", "7.727682394E9", "ks_21285@hotmail.com"))));
 
     }
+
+
+
+    private boolean contains(List<String> list1, List<String> list2) {
+        boolean found = true;
+        for (String item : list2) {
+            if (!list1.contains(item)) {
+                found = false;
+                System.out.println(item);
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println(list1 +"\n" + list2);
+        }
+        return found;
+    }
+
 }
