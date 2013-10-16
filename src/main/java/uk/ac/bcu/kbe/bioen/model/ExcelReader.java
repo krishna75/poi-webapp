1ab0024ca7a47b4f00da327d0aa0014a28a38956
Krishna Sapkota <krishna.sapkota@bcu.ac.uk>
2013-10-16 17:25:12 +0100

    including important files in git

---
+++ src/main/java/uk/ac/bcu/kbe/bioen/model/ExcelReader.java
@@ -0,0 +1,74 @@
+package uk.ac.bcu.kbe.bioen.model;
+
+
+import com.google.common.collect.Lists;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+
+/**
+* Created with IntelliJ IDEA.
+* User: ID118320
+* Date: 11/10/13
+* Time: 12:10
+* To change this template use File | Settings | File Templates.
+*/
+
+public class ExcelReader {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+    private final XSSFWorkbook workbook;
+
+    public ExcelReader(String filename) throws IOException {
+        workbook = process(filename);
+    }
+
+    XSSFWorkbook process(String filename) throws IOException {
+        FileInputStream file = new FileInputStream(new File(filename));
+        return new XSSFWorkbook(file);
+    }
+
+
+    public List<String> getRowValues(int sheetIndex, int rowIndex) {
+        List<String>  rowValues = Lists.newArrayList();
+
+        XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
+        Row row = sheet.getRow(rowIndex);
+        Iterator<Cell> cellIterator = row.cellIterator();
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            rowValues.add(getValue(cell));
+        }
+        return rowValues;
+    }
+
+    public String getCellValue(int sheetIndex, int rowIndex, int columnIndex) {
+        XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
+        Cell cell = sheet.getRow(rowIndex).getCell(columnIndex);
+        return  getValue(cell);
+    }
+
+    private String getValue(Cell cell) {
+        String cellValue = "";
+        if (cell != null) {
+            switch (cell.getCellType()) {
+                case Cell.CELL_TYPE_NUMERIC:
+                   cellValue= String.valueOf(cell.getNumericCellValue());
+                    break;
+                case Cell.CELL_TYPE_STRING:
+                    cellValue = cell.getStringCellValue();
+                    break;
+            }
+        }
+        return cellValue;
+    }
+}
