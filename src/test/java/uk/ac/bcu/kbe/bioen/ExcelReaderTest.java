e2f84bce16c3c034013ee2b2f44efa67ea632197
Krishna Sapkota <krishna.sapkota@bcu.ac.uk>
2013-10-16 17:19:13 +0100

    getting a cell's value

--- src/test/java/uk/ac/bcu/kbe/bioen/ExcelReaderTest.java
+++ src/test/java/uk/ac/bcu/kbe/bioen/ExcelReaderTest.java
@@ -2,10 +2,9 @@ package uk.ac.bcu.kbe.bioen;
 
 import com.google.common.collect.Lists;
 import org.junit.Test;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 import uk.ac.bcu.kbe.bioen.model.ExcelReader;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -22,17 +21,20 @@ import static org.junit.Assert.assertTrue;
 
 
 public class ExcelReaderTest {
+    String filename = this.getClass().getClassLoader().getResource("test.xlsx").getFile();
 
     @Test
     public void testGetRowValues() throws Exception {
-
-        String filename = this.getClass().getClassLoader().getResource("test.xlsx").getFile();
-
         assertTrue(contains(new ExcelReader(filename).getRowValues(0, 4)
                 ,(Lists.newArrayList("Name", "Address", "Phone", "Email"))));
         assertTrue(contains(new ExcelReader(filename).getRowValues(0, 5)
                 ,(Lists.newArrayList("Krishna Sapkota", "303 Hollow Way", "7.727682394E9", "ks_21285@hotmail.com"))));
 
+    }
+
+    @Test
+    public void testCellValue() throws IOException {
+       assertEquals("Krishna Sapkota",new  ExcelReader(filename).getCellValue(0,5,0));
     }
 
 
